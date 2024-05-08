58f5f39162c85c291ba9cb7d8e6111a4117f2091
_ilia_kravtsov <kravtsov.i.s@mail.ru>
2024-05-08 21:07:01 +0400

    pc done version

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-
+new Swiper(".main__fourth-swiper", {
+  direction: "horizontal",
+  slidesPerView: 3,
+  spaceBetween: 94,
+  loop: true,
+  autoplay: {
+    delay: 4000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".main__fourth-nav-pagination",
+    type: "fraction",
+  },
+  navigation: {
+    nextEl: ".main__fourth-nav-next",
+    prevEl: ".main__fourth-nav-prev",
+    disabledClass: "swiper-button-disabled",
+  },
+});
